14df2ad85edb489af9169f94668d536e057d3192
glz <glzztt@gmail.com>
2017-07-28 20:18:15 +0800

    activate users specifically

--- lib/rollout.rb
+++ lib/rollout.rb
@@ -5,15 +5,19 @@ class Rollout
     end
 
     def activate_group(feature, group)
-        @redis.sadd(key(feature), group)
+        @redis.sadd(group_key(feature), group)
     end
 
     def deactivate_group(feature, group)
-        @redis.srem(key(feature), group)
+        @redis.srem(group_key(feature), group)
+    end
+
+    def activate_user(feature, user)
+        @redis.sadd(user_key(feature), user.id)
     end
 
     def deactivate_all(feature)
-        @redis.del(key(feature))
+        @redis.del(group_key(feature))
     end
 
     def define_group(group, &block)
@@ -21,10 +25,27 @@ class Rollout
     end
 
     def active?(feature, user)
-        @redis.smembers(key(feature)).any? { |group| @groups[group].call(user) }
+        # @redis.smembers(group_key(feature)).any? { |group| @groups[group].call(user) }
+        user_in_active_group?(feature, user) || user_active?(feature, user)
     end
 
     private def key(name)
         "feature:#{name}"
+    end
+
+    private def group_key(name)
+        "#{key(name)}:groups"
+    end
+
+    private def user_key(name)
+        "#{key(name)}:users"
+    end
+
+    private def user_in_active_group?(feature, user)
+        @redis.sismember(user_key(feature), user.id)
+    end
+
+    private def user_in_active_group?(feature, user)
+        @redis.smembers(group_key(feature)).any? { |group| @group[group.to_s].call(user) }
     end
 end
