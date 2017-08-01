97ce52f6835fd5b7af7de12689583f5d9dc1800b
glz <glzztt@gmail.com>
2017-08-01 21:58:50 +0800

    add Rollout::Feature#to_hash

--- lib/rollout.rb
+++ lib/rollout.rb
@@ -53,6 +53,14 @@ class Rollout
             end
         end
 
+        def to_hash
+            {
+                :percentage   => @percentage,
+                :groups         => @groups,
+                :users           => @users
+            }
+        end
+
         private def user_in_percentage?(user)
             user.id % 100 < @percentage
         end
