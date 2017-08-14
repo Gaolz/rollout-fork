f11037a5b74d211cd57d1ddff4247113b578804f
glz <glzztt@gmail.com>
2017-08-15 07:51:21 +0800

    Update dependencies

--- rollout.gemspec
+++ rollout.gemspec
@@ -22,4 +22,5 @@ Gem::Specification.new do |s|
   s.add_development_dependency  "pry"
   s.add_development_dependency "redis"
   s.add_development_dependency "fakeredis"
+  s.add_development_dependency "simplecov"
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,10 +2,17 @@ $LOAD_PATH.unshift(File.dirname(__FILE__))
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 
 require 'rollout'
+require "simplecov"
 require 'rspec'
 require 'redis'
 require ENV["USE_REAL_REDIS"] == "true" ? "redis" : "fakeredis"
 require 'pry'
+
+SimpleCov.start do
+    formatter SimpleCov::Formatter::MultiFormatter.new([
+        SimpleCov::Formatter::HTMLFormatter,
+    ])
+end
 
 RSpec.configure do |config|
     config.before { Redis.new.flushdb }
